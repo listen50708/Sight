c1eb39ecd829fc636e9fd66205190b94be845116
張力升 <jc91110@gmailcom>
2020-06-18 22:44:35 +0800

    final

--- Sight/src/listener/ServletListener.java
+++ Sight/src/listener/ServletListener.java
@@ -41,13 +41,13 @@ public class ServletListener implements ServletContextListener {
 		
 		try {
 			long startTime = System.currentTimeMillis();
-			//ArrayList<Sight> taipeiSights = crawler.getItems("taipei-city/");
-			//sc.setAttribute("taipei", taipeiSights);
+			ArrayList<Sight> taipeiSights = crawler.getItems("taipei-city/");
+			sc.setAttribute("taipei", taipeiSights);
 			ArrayList<Sight> newtaipeiSights = crawler.getItems("newtaipei-city/");
 			sc.setAttribute("newtaipei", newtaipeiSights);
 			ArrayList<Sight> taoyuanSights = crawler.getItems("taoyuan-city/");
 			sc.setAttribute("taoyuan", taoyuanSights);
-			/*ArrayList<Sight> taichungSights = crawler.getItems("taichung-city/");
+			ArrayList<Sight> taichungSights = crawler.getItems("taichung-city/");
 			sc.setAttribute("taichung", taichungSights);
 			ArrayList<Sight> tainanSights = crawler.getItems("tainan-city/");
 			sc.setAttribute("tainan", tainanSights);
@@ -85,7 +85,7 @@ public class ServletListener implements ServletContextListener {
 			sc.setAttribute("kinmencounty", kinmencountySights);
 			ArrayList<Sight> lienchiangcountySights = crawler.getItems("lienchiangcounty/");
 			sc.setAttribute("lienchiangcounty", lienchiangcountySights);
-			*/
+			
 			System.out.println("Using Time:" + (System.currentTimeMillis() - startTime) + " ms");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
